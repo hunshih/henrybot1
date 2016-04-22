79602d4b54e267704c7e9e9096d214a1f33ebba1
unknown <hunshih@umich.edu>
2016-04-22 01:04:04 -0400

    add challenge

--- index.js
+++ index.js
@@ -18,8 +18,11 @@ app.listen(app.get('port'), function() {
 });
 
 app.get('/webhook', function(req,res){
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Page One');
+   if (req.query['hub.verify_token'] === 'CAADezUPr1f4BAAJK2uB7UZCDm5eJtKE5xLdP5FZCFzHoNM7MQHfKDXdHHci0qFj3aGIhHBSOsxBfGudFu7jhvXRzVBK284dD7rr0tuveFjtBEMxDqVwDOBiSJM7SytpXGJrY2i3wlZBZAZCxlV4GzbwXtuGfmlkhD6xituWUHLu64saY8rIZAm6FSqZC9YRqRlcJwMIPTus5gZDZD') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');    
+  } 
 });
 
 
