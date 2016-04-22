e4d2cd5b8ed1b751ddbb23038698472c61413470
unknown <hunshih@umich.edu>
2016-04-22 00:44:18 -0400

    used template node

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-//Lets require/import the HTTP module
-var http = require('http');
+/**
+ * Module dependencies.
+ */
 
-//allow different handler for different path
-var dispatcher = require('httpdispatcher');
+var express = require('express'),
+  routes = require('./routes'),
+  user = require('./routes/user'),
+  http = require('http'),
+  path = require('path');
 
-//Lets define a port we want to listen to
-const PORT=8080; 
+var app = express();
 
-//We need a function which handles requests and send response
-//Lets use our dispatcher
-function handleRequest(request, response){
-    try {
-        //log the request on console
-        console.log(request.url);
-        //Disptach
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-//For all your static (js/css/images/etc.) set the directory name (relative path).
-dispatcher.setStatic('resources');
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
-//A sample GET request    
-dispatcher.onGet("/webhook", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Page One');
-});    
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
+});
 
-//A sample POST request
-dispatcher.onPost("/post1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Got Post Data');
+app.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
