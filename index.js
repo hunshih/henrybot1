ae037992852e142eb93c952a34ed4fbb46f3aa4a
unknown <hunshih@umich.edu>
2016-04-22 00:51:24 -0400

    try original example

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//Lets define a port we want to listen to
-const PORT=3000; 
+app.set('port', (process.env.PORT || 5000));
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
+app.use(express.static(__dirname + '/public'));
 
-//Create a server
-var server = http.createServer(handleRequest);
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/webhook', function(req,res){
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});
+
+
