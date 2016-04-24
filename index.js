91c84ab75dcd7454ebdc293925c58dc073e545f9
unknown <hunshih@umich.edu>
2016-04-24 16:59:55 -0400

    getting body message

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/webhook', function(req, res) {
     //res.send('Webhook POST working');
     //var msg = JSON.parse(req);
     //console.log(msg.entry.messaging.message.text);
-    console.log(req.entry);
+    console.log(req.body.entry.messaging.message.text);
     //postRequest();
 });
 
