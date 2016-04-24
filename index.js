9b2a392c97f58496ac4e9ba84a106a2a32c51ec2
unknown <hunshih@umich.edu>
2016-04-24 19:27:20 -0400

    logging first messagw

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post('/webhook', function(req, res) {
     //res.send('Webhook POST working');
     //var msg = JSON.parse(req);
     //console.log(msg.entry.messaging.message.text);
-    console.log(req.body.entry.messaging.message.text);
+    console.log(req.body.entry[0].messaging[0].message.text);
     //console.log("Testing");
     //postRequest();
 });
