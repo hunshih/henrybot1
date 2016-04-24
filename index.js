a1c7d599abe8d81c1fb60ac05f28650fd2fdacff
unknown <hunshih@umich.edu>
2016-04-24 19:48:04 -0400

    print first entry

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ app.post('/webhook', function(req, res) {
     //res.send('Webhook POST working');
     //var msg = JSON.parse(req);
     //console.log(msg.entry.messaging.message.text);
-    console.log(req.body.entry[0]);
+    for(var a = 0; a< (req.body.entry[0].messaging[0].message).length;a++){
+        console.log(req.body.entry[0].messaging[0].message[a].text);
+    }
+    
+    res.send("done");
     //console.log("Testing");
     //postRequest();
 });
