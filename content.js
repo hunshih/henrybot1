59c0856861ae016e80bf7cb1f4de44213ebae0ec
unknown <hunshih@umich.edu>
2016-04-27 23:55:21 -0400

    try adding function not in module

--- content.js
+++ content.js
@@ -1,8 +1,16 @@
 module.exports = {
   ResponseMessage: function () {
-    return 'Hi how are you';
+    return getName();
   },
   bar: function () {
     // whatever
   }
 };
+
+var getMarketCap = function(symbol){
+    return "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + symbol + "%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+}
+
+var getName = function(){
+    return 'Henry';
+}
