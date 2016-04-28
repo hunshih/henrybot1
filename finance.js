c0703a027f57483447684ea55fc333d4c27e5844
unknown <hunshih@umich.edu>
2016-04-28 00:34:27 -0400

    send company name

--- finance.js
+++ finance.js
@@ -21,11 +21,12 @@ var getCompanyName = function(id,ticker){
     request(getMarketCap(ticker), function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var markteJson = JSON.parse(body);
-            /*_companyName = markteJson.query.results.quote.Name;
-            _sharePrice = markteJson.query.results.quote.LastTradePriceOnly;
+            var companyName = markteJson.query.results.quote.Name;
+            /*_sharePrice = markteJson.query.results.quote.LastTradePriceOnly;
             _marketCapString =
                 markteJson.query.results.quote.MarketCapitalization;
             _marketCap = convertMarketCap(_marketCapString);*/
+            send_response(id,companyName);
         }
         else {
             //console.log(error) // Print the shortened url.
